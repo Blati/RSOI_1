c0d2a5243a1ff5be789608a6b3e68e13ebf64722
Blati <blati1996@gmail.com>
2018-11-21 03:24:03 +0300

    Changed.

--- RSOI_1/RSOI_1/settings.py
+++ RSOI_1/RSOI_1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3arf^48)1i4!okk!2&n^x2h03%-$xz*%p9j6yr*!)a@%(=!pgt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.0', 'rsoi-1.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.0', 'firstlabrsoi.herokuapp.com']
 
 
 # Application definition
